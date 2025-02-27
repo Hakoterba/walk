61e816fed16c1ae7e883137cddb606bf886f41dc
hakoterba <koterbah@gmail.com>
2025-02-27 23:43:44 +0100

    navbar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,42 @@
+import { Tabs } from 'expo-router';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+export default function TabLayout() {
+    return (
+        <Tabs
+        screenOptions={{
+            tabBarActiveTintColor: '#1E1E1E',
+        }}
+        >
+        <Tabs.Screen
+            name="index"
+            options={{
+            title: 'Accueil',
+            tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+            ),
+            }}
+        />
+        <Tabs.Screen
+            name="parcours"
+            options={{
+            title: 'Parcours',
+            tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'location-sharp' : 'location-outline'} color={color} size={24}/>
+            ),
+            }}
+        />
+        <Tabs.Screen
+            name="profil"
+            options={{
+            title: 'Profil',
+            tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'person' : 'person-outline'} color={color} size={24}/>
+            ),
+            }}
+        />
+        </Tabs>
+    );
+}
